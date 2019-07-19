2d69cba55b29c68ca3f02d3164727ada4332e4df
windam <windam@live>
2019-07-19 18:40:31 +0530

    create a UserForm and import in app.js

---
+++ src/components/UserForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UserForm() {
+  return (
+    <form>
+      <input type='text' name='username' />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+}
+
+export default UserForm;
