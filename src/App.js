b03a71967778935fcf193789f99a64d9144d1c82
windam <windam@live>
2019-07-19 19:13:37 +0530

    get data from api an display it

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 import UserForm from "./components/UserForm";
 
-function App() {
-  const getUser = e => {
+class App extends Component {
+  state = {
+    repos: null
+  };
+
+  getUser = e => {
     e.preventDefault();
     // console.log(user);
     const user = e.target.elements.username.value;
     const url = `https://api.github.com/users/${user}`;
     axios.get(url).then(res => {
-      console.log(res);
+      // console.log(res);
+      const repos = res.data.public_repos;
+      // console.log(repos);
+      this.setState({
+        repos
+      });
     });
   };
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>HTTP Calls in React</h1>
-      </header>
-      <UserForm getUser={getUser} />
-    </div>
-  );
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>HTTP Calls in React</h1>
+        </header>
+        <UserForm getUser={this.getUser} />
+        {this.state.repos ? (
+          <p>Number of repose: {this.state.repos}</p>
+        ) : (
+          <p>Please enter a username</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
