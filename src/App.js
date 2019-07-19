97129200ce41fb6bb7bdc146a94c259cbac59342
windam <windam@live>
2019-07-19 18:46:40 +0530

    create getUser function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./App.css";
 import UserForm from "./components/UserForm";
 
 function App() {
+  const getUser = e => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    console.log(user);
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>HTTP Calls in React</h1>
       </header>
-      <UserForm />
+      <UserForm getUser={getUser} />
     </div>
   );
 }
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-function UserForm() {
+function UserForm(props) {
   return (
-    <form>
-      <input type='text' name='username' />
+    <form onSubmit={props.getUser}>
+      <input
+        style={{ margin: "20px auto", display: "block" }}
+        type='text'
+        name='username'
+      />
       <button type='submit'>Submit</button>
     </form>
   );
